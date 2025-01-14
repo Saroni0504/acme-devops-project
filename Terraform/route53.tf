49fef9cf866458f122d81ce5fa9b69d595b7393e
Saroni0504 <85100855+Saroni0504@users.noreply.github.com>
2025-01-14 19:04:22 +0200

    Update route53.tf

--- Terraform/route53.tf
+++ Terraform/route53.tf
@@ -1,10 +1,10 @@
-resource "aws_route53_zone" "acme" {
-  name = "acme-roni.com"
+resource "aws_route53_zone" "project" {
+  name = "project-roni.com"
 }
 resource "aws_route53_record" "cname" {
-  zone_id = aws_route53_zone.acme.zone_id
-  name    = "www.acme-roni.com"
+  zone_id = aws_route53_zone.project.zone_id
+  name    = "www.project-roni.com"
   type    = "CNAME"
   ttl     = 300
-  records = ["${aws_elb.elb_acme.dns_name}"]
+  records = ["${aws_elb.elb.dns_name}"]
 }
